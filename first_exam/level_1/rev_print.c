919ad1c825e6dbdae5610a1f121d5e66e8df3600
Violinie <hancacopakova@gmail.com>
2025-02-12 12:25:04 +0100

    add rev_print

---
+++ first_exam/level_1/rev_print.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_print.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanacop <hanacop@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 12:19:18 by hanacop           #+#    #+#             */
+/*   Updated: 2025/02/12 12:19:18 by hanacop          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*Assignment name  : rev_print
+Expected files   : rev_print.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string, and displays the string in reverse
+followed by a newline.
+
+If the number of parameters is not 1, the program displays a newline.
+
+Examples:
+
+$> ./rev_print "zaz" | cat -e
+zaz$
+$> ./rev_print "dub0 a POIL" | cat -e
+LIOP a 0bud$
+$> ./rev_print | cat -e
+$*/
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+			i++;
+		}
+		while (i >= 0)
+		{
+			write(1, &argv[1][i], 1);
+			i--;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
