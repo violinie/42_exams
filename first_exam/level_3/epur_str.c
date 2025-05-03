73035c32f39cbb29de088eb80c783fb61b9cd78e
Violinie <hancacopakova@gmail.com>
2025-05-03 12:51:44 +0200

    add epur_str

---
+++ first_exam/level_3/epur_str.c
@@ -0,0 +1,52 @@
+/*Assignment name  : epur_str
+Expected files   : epur_str.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string, and displays this string with exactly one
+space between words, with no spaces or tabs either at the beginning or the end,
+followed by a \n.
+
+A "word" is defined as a part of a string delimited either by spaces/tabs, or
+by the start/end of the string.
+
+If the number of arguments is not 1, or if there are no words to display, the
+program displays \n.
+
+Example:
+
+$> ./epur_str "vous voyez c'est facile d'afficher la meme chose" | cat -e
+vous voyez c'est facile d'afficher la meme chose$
+$> ./epur_str " seulement          la c'est      plus dur " | cat -e
+seulement la c'est plus dur$
+$> ./epur_str "comme c'est cocasse" "vous avez entendu, Mathilde ?" | cat -e
+$
+$> ./epur_str "" | cat -e
+$
+$>*/
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i = 0;
+	if(argc == 2)
+	{
+		while(argv[1][i] == ' ')
+				i++;
+		while(argv[1][i])
+		{
+			if (argv[1][i] == ' ')
+			{
+				if (argv[1][i + 1] != ' ' && argv[1][i + 1] != '\0')
+					write(1, &argv[1][i], 1);
+			}
+			else
+				write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return(0);
+}
+
